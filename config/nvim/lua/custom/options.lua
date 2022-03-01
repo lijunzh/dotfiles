74e917e0a3c8f9320b2303004b430a78e2f211af
Lijun Zhu <gatechzhu@gmail.com>
2022-03-01 17:01:15 -0600

    Revert "Define custom options directly."
    This reverts commit b08363fa246f8bf3a71cea4699fe3f2d327bf0b7.

--- config/nvim/lua/custom/options.lua
+++ config/nvim/lua/custom/options.lua
@@ -5,13 +5,13 @@ local options = require("core.utils").load_config().options
 --------------------------------------------------------------------------------
 
 -- Map local leader key
-vim.g.maplocalleader = ","
+vim.g.maplocalleader = options.maplocalleader
 
 -- have some line above and below cursor
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = options.scrolloff
 
 -- control backspace behavior
-vim.opt.backspace = ""
+vim.opt.backspace = options.backspace
 
 -- file extension specific tabbing
 vim.cmd([[autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4]])
@@ -21,21 +21,21 @@ vim.cmd([[autocmd Filetype lua setlocal expandtab tabstop=3 shiftwidth=3 softtab
 -- appearance
 --------------------------------------------------------------------------------
 
-vim.opt.showmatch = true
+vim.opt.showmatch = options.showmatch
 
 -- line wrap
-vim.opt.wrap = false
-vim.opt.linebreak = true
-vim.opt.showbreak = "↪"
+vim.opt.wrap = options.wrap
+vim.opt.linebreak = options.linebreak
+vim.opt.showbreak = options.showbreak
 
 -- list
-vim.opt.list = true
-vim.opt.listchars = "tab:→ ,eol:¬,trail:⋅,extends:❯,precedes:❮,space:·"
+vim.opt.list = options.list
+vim.opt.listchars = options.listchars
 
 -- 79 column line
 -- excpetions for git and text file are defined in utils.lua
-vim.opt.textwidth = 79
-vim.opt.colorcolumn = "+1"
+vim.opt.textwidth = options.textwidth
+vim.opt.colorcolumn = options.colorcolumn
 
 -- highlight on yank
 vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank()")
