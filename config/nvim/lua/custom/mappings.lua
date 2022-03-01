55f41d61a180762bff505d0b897d04c1d0e5ca11
Lijun Zhu <gatechzhu@gmail.com>
2022-03-01 15:18:12 -0600

    Define format mapping in null-ls.

--- config/nvim/lua/custom/mappings.lua
+++ config/nvim/lua/custom/mappings.lua
@@ -6,6 +6,3 @@ map("n", "<leader>q", ":q <CR>")
 -- packer
 map("n", "<C-p>c", ":PackerClean <CR>")
 map("n", "<C-p>s", ":PackerSync <CR>")
-
--- always have mappings for formatting code
-map("n", "<leader>fm", ":lua vim.lsp.buf.formatting()<CR>")
--- config/nvim/lua/custom/plugins/null-ls.lua
+++ config/nvim/lua/custom/plugins/null-ls.lua
@@ -21,6 +21,13 @@ M.setup = function()
 	null_ls.setup({
 		debug = true,
 		sources = sources,
+		on_attach = function(client)
+			if client.resolved_capabilities.document_formatting then
+				vim.cmd([[
+               nmap <leader>fm :lua vim.lsp.buf.formatting_sync()<CR>
+            ]])
+			end
+		end,
 	})
 end
 
