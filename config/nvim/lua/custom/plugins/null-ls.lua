dbec33889a29d2de4517df8ac5c63d5b06d78d4b
Lijun Zhu <gatechzhu@gmail.com>
2022-03-02 08:48:28 -0600

    Fix a typo.

--- config/nvim/lua/custom/plugins/null-ls.lua
+++ config/nvim/lua/custom/plugins/null-ls.lua
@@ -24,7 +24,7 @@ M.setup = function()
 		on_attach = function(client)
 			if client.resolved_capabilities.document_formatting then
 				vim.cmd([[
-					nnmap <leader>fm :lua vim.lsp.buf.formatting_sync()<CR>
+					nmap <leader>fm :lua vim.lsp.buf.formatting_sync()<CR>
 				]])
 			end
 		end,
