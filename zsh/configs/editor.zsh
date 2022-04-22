58c3f4691ba2d3f6887b9f89c14472d2141bebe9
Lijun Zhu <lijunzh@users.noreply.github.com>
2022-04-22 11:10:49 -0500

    Update editor.zsh

--- zsh/configs/editor.zsh
+++ zsh/configs/editor.zsh
@@ -2,10 +2,3 @@ if hash nvim 2>/dev/null; then
 	export VISUAL=nvim
 fi;
 export EDITOR=$VISUAL
-
-# if [ -e /usr/share/terminfo/x/xterm-256color ]; then
-# 	export TERM='xterm-256color'
-# else
-# 	export TERM='xterm-color'
-# fi
-
