cd0334425f5af55f45cbc09434657115089dae47
Lijun Zhu <gatechzhu@gmail.com>
2022-05-09 14:44:27 -0500

    Revert "Update keybindings.zsh"
    This reverts commit 235cb47eabd12b0f698d63771fe8b234cee25ea6.

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -5,6 +5,9 @@ stty -ixon
 bindkey -v
 bindkey "^F" vi-cmd-mode
 
+# Make Vi mode transitions faster (KEYTIMEOUT is in hundredths of a second)
+export KEYTIMEOUT=1
+
 # handy keybindings
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
@@ -15,3 +18,19 @@ bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
 bindkey "^Q" push-line-or-edit
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
+  zle reset-prompt
+  zle -R
+}
+
+zle -N zle-keymap-select
+
+function vi_mode_prompt_info() {
+  echo "${${KEYMAP/vicmd/[% NORMAL]%}/(main|viins)/[% INSERT]%}"
+}
+
+# define right prompt, regardless of whether the theme defined it
+RPS1='$(vi_mode_prompt_info)'
+RPS2=$RPS1
