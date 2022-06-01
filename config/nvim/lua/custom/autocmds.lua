16dee1d6a31d7984586129f7ed92067790ffc3ef
Lijun Zhu <gatechzhu@gmail.com>
2022-06-01 14:52:59 -0500

    Fix white spaces.

--- config/nvim/lua/custom/autocmds.lua
+++ config/nvim/lua/custom/autocmds.lua
@@ -2,9 +2,9 @@
 vim.api.nvim_exec(
 	[[
 augroup file_change
-   autocmd!
-   autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif
-   autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
+	autocmd!
+	autocmd FocusGained,BufEnter,CursorHold,CursorHoldI * if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif
+	autocmd FileChangedShellPost * echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None
 augroup END
 ]],
 	false
@@ -14,9 +14,9 @@ augroup END
 vim.api.nvim_exec(
 	[[
 augroup auto_save_remove_white_space
-   autocmd!
-   autocmd BufWritePre * silent! s/\s\+$//e
-   autocmd BufWritePre * silent! %s#\($\n\s*\)\+\%$##
+	autocmd!
+	autocmd BufWritePre * silent! s/\s\+$//e
+	autocmd BufWritePre * silent! %s#\($\n\s*\)\+\%$##
 augroup END
 ]],
 	false
@@ -26,11 +26,11 @@ augroup END
 vim.api.nvim_exec(
 	[[
 augroup column_width
-   autocmd!
-   " no text wrap for text files
-   autocmd Filetype tex,plaintex,markdown,text setlocal tw=0 wrap
-   " 72 columns for git commit
-   autocmd Filetype gitcommit setlocal spell textwidth=72
+	autocmd!
+	" no text wrap for text files
+	autocmd Filetype tex,plaintex,markdown,text setlocal tw=0 wrap
+	" 72 columns for git commit
+	autocmd Filetype gitcommit setlocal spell textwidth=72
 augroup END
 ]],
 	false
@@ -40,57 +40,57 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- Open nvim with a dir
 autocmd("BufEnter", {
-   callback = function()
-      if vim.api.nvim_buf_get_option(0, "buftype") ~= "terminal" then
-         vim.cmd "lcd %:p:h"
-      end
-   end,
+	callback = function()
+		if vim.api.nvim_buf_get_option(0, "buftype") ~= "terminal" then
+			vim.cmd "lcd %:p:h"
+		end
+	end,
 })
 
 -- Use relative & absolute line numbers in 'n' & 'i' modes respectively
 autocmd("InsertEnter", {
-   callback = function()
-      vim.opt.relativenumber = false
-   end,
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
 })
 autocmd("InsertLeave", {
-   callback = function()
-      vim.opt.relativenumber = true
-   end,
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
 })
 
 -- Open a file from its last left off position
 autocmd("BufReadPost", {
-   callback = function()
-      if not vim.fn.expand("%:p"):match ".git" and vim.fn.line "'\"" > 1 and vim.fn.line "'\"" <= vim.fn.line "$" then
-         vim.cmd "normal! g'\""
-         vim.cmd "normal zz"
-      end
-   end,
+	callback = function()
+		if not vim.fn.expand("%:p"):match ".git" and vim.fn.line "'\"" > 1 and vim.fn.line "'\"" <= vim.fn.line "$" then
+			vim.cmd "normal! g'\""
+			vim.cmd "normal zz"
+		end
+	end,
 })
 
 -- File extension specific tabbing
 autocmd("Filetype", {
-   pattern = "python",
-   callback = function()
-      vim.opt_local.expandtab = true
-      vim.opt_local.tabstop = 4
-      vim.opt_local.shiftwidth = 4
-      vim.opt_local.softtabstop = 4
-   end,
+	pattern = "python",
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.softtabstop = 4
+	end,
 })
 
 -- Highlight yanked text
 autocmd("TextYankPost", {
-   callback = function()
-      vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
-   end,
+	callback = function()
+		vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+	end,
 })
 
 -- Enable spellchecking in markdown, text and gitcommit files
 autocmd("FileType", {
-   pattern = { "gitcommit", "markdown", "text" },
-   callback = function()
-      vim.opt_local.spell = true
-   end,
+	pattern = { "gitcommit", "markdown", "text" },
+	callback = function()
+		vim.opt_local.spell = true
+	end,
 })
