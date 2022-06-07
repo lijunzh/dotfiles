3072b38611f01bf72fa6d2ae28cb6d2519aecbe5
Lijun Zhu <gatechzhu@gmail.com>
2022-06-07 09:42:51 -0500

    Fix formating

--- config/nvim/lua/custom/plugins/null-ls.lua
+++ config/nvim/lua/custom/plugins/null-ls.lua
@@ -1,43 +1,42 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local sources = {
 
-   -- cpp
-   b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 
-   -- webdev stuff
-   b.formatting.deno_fmt,
-   b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "astro" } },
+	-- webdev stuff
+	b.formatting.deno_fmt,
+	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css", "astro" } }),
 
-   -- Lua
-   b.formatting.stylua,
-   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+	-- Lua
+	b.formatting.stylua,
+	b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }),
 
-   -- python
+	-- python
 	b.formatting.black,
 	b.formatting.isort,
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
-
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 }
 
 local M = {}
 
 M.setup = function()
-   null_ls.setup {
-      debug = true,
-      sources = sources,
-      on_attach = function(client)
+	null_ls.setup({
+		debug = true,
+		sources = sources,
+		on_attach = function(client)
 			if client.resolved_capabilities.document_formatting then
 				vim.cmd([[
 					nmap <leader>fm :lua vim.lsp.buf.formatting_sync()<CR>
 				]])
 			end
 		end,
-   }
+	})
 end
 
 return M
