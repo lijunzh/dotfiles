905771c0938942ada390084ed15766bc4860e5ef
Lijun Zhu <gatechzhu@gmail.com>
2022-06-21 15:24:52 -0500

    Adapt to new NvChad version.
    Move usage of `require` from override.lua to a function in `chadrc.lua`.

--- config/nvim/lua/custom/chadrc.lua
+++ config/nvim/lua/custom/chadrc.lua
@@ -14,7 +14,56 @@ M.plugins = {
 	},
 
 	override = {
-		["hrsh7th/nvim-cmp"] = override.cmp,
+		["hrsh7th/nvim-cmp"] = function()
+			local cmp = require "cmp"
+			local source_mapping = {
+				buffer = "[Buffer]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[Lua]",
+				cmp_tabnine = "[TN]",
+				path = "[Path]",
+			}
+			return {
+				sources = {
+					 { name = "cmp_tabnine" },
+					 { name = "nvim_lsp" },
+					 { name = "luasnip" },
+					 { name = "buffer" },
+					 { name = "nvim_lua" },
+					 { name = "path" },
+				 },
+			 
+				 formatting = {
+					 format = function(entry, vim_item)
+						 local icons = require("plugins.configs.lspkind_icons")
+						 vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+			 
+						 local menu = source_mapping[entry.source.name]
+						 if entry.source.name == "cmp_tabnine" then
+							 if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+								 menu = entry.completion_item.data.detail .. " " .. menu
+							 end
+							 vim_item.kind = ""
+						 end
+						 vim_item.menu = menu
+			 
+						 return vim_item
+					 end,
+				 },
+			 
+				 mapping = {
+					 ["<C-p>"] = cmp.mapping.select_prev_item(),
+					 ["<C-n>"] = cmp.mapping.select_next_item(),
+					 ["<C-e>"] = cmp.mapping.close(),
+					 ["<CR>"] = cmp.mapping.confirm({
+						 behavior = cmp.ConfirmBehavior.Replace,
+						 select = true,
+					 }),
+					 ["<Tab>"] = cmp.config.disable,
+					 ["<S-Tab>"] = cmp.config.disable,
+				 },
+			 }
+		end,
 		["kyazdani42/nvim-tree.lua"] = override.nvimtree,
 		["nvim-treesitter/nvim-treesitter"] = override.treesitter,
 	},
--- config/nvim/lua/custom/plugins/override.lua
+++ config/nvim/lua/custom/plugins/override.lua
@@ -1,13 +1,13 @@
 -- overriding default plugin configs!
 
-local cmp = require "cmp"
-local source_mapping = {
-	buffer = "[Buffer]",
-	nvim_lsp = "[LSP]",
-	nvim_lua = "[Lua]",
-	cmp_tabnine = "[TN]",
-	path = "[Path]",
-}
+-- local cmp = require "cmp"
+-- local source_mapping = {
+-- 	buffer = "[Buffer]",
+-- 	nvim_lsp = "[LSP]",
+-- 	nvim_lua = "[Lua]",
+-- 	cmp_tabnine = "[TN]",
+-- 	path = "[Path]",
+-- }
 local M = {}
 
 M.treesitter = {
@@ -92,47 +92,72 @@ M.nvimtree = {
    git = {
       enable = true,
    },
-}
 
-M.cmp = {
-   sources = {
-		{ name = "cmp_tabnine" },
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "nvim_lua" },
-		{ name = "path" },
+	renderer = {
+	highlight_git = true,
+	icons = {
+		show = {
+			git = true,
+		},
 	},
-
-	formatting = {
-		format = function(entry, vim_item)
-			local icons = require("plugins.configs.lspkind_icons")
-			vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
-
-			local menu = source_mapping[entry.source.name]
-			if entry.source.name == "cmp_tabnine" then
-				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-					menu = entry.completion_item.data.detail .. " " .. menu
-				end
-				vim_item.kind = ""
-			end
-			vim_item.menu = menu
-
-			return vim_item
-		end,
-	},
-
-	mapping = {
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.config.disable,
-		["<S-Tab>"] = cmp.config.disable,
 	},
 }
+
+M.blankline = {
+	filetype_exclude = {
+	   "help",
+	   "terminal",
+	   "alpha",
+	   "packer",
+	   "lspinfo",
+	   "TelescopePrompt",
+	   "TelescopeResults",
+	   "nvchad_cheatsheet",
+	   "lsp-installer",
+	   "norg",
+	   "",
+	},
+ }
+
+-- M.cmp = {
+--    sources = {
+-- 		{ name = "cmp_tabnine" },
+-- 		{ name = "nvim_lsp" },
+-- 		{ name = "luasnip" },
+-- 		{ name = "buffer" },
+-- 		{ name = "nvim_lua" },
+-- 		{ name = "path" },
+-- 	},
+
+-- 	formatting = {
+-- 		format = function(entry, vim_item)
+-- 			local icons = require("plugins.configs.lspkind_icons")
+-- 			vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+
+-- 			local menu = source_mapping[entry.source.name]
+-- 			if entry.source.name == "cmp_tabnine" then
+-- 				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+-- 					menu = entry.completion_item.data.detail .. " " .. menu
+-- 				end
+-- 				vim_item.kind = ""
+-- 			end
+-- 			vim_item.menu = menu
+
+-- 			return vim_item
+-- 		end,
+-- 	},
+
+-- 	mapping = {
+-- 		["<C-p>"] = cmp.mapping.select_prev_item(),
+-- 		["<C-n>"] = cmp.mapping.select_next_item(),
+-- 		["<C-e>"] = cmp.mapping.close(),
+-- 		["<CR>"] = cmp.mapping.confirm({
+-- 			behavior = cmp.ConfirmBehavior.Replace,
+-- 			select = true,
+-- 		}),
+-- 		["<Tab>"] = cmp.config.disable,
+-- 		["<S-Tab>"] = cmp.config.disable,
+-- 	},
+-- }
 
 return M
