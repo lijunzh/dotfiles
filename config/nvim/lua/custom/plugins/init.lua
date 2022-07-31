cef584503a291a1eccadb573d5adafc58fc255ca
Lijun Zhu <lijunzh@users.noreply.github.com>
2022-07-31 11:11:02 -0500

    Fix autosave error (#2)
    * Fix a typo in `install.sh`.
    
    * Update config for autosave plugin.

--- config/nvim/lua/custom/plugins/init.lua
+++ config/nvim/lua/custom/plugins/init.lua
@@ -1,22 +1,9 @@
 return {
 	-- autosave
 	["Pocco81/AutoSave.nvim"] = {
+    module = "autosave",
 		config = function()
-			local autosave = require("autosave")
-
-			autosave.setup({
-				enabled = true,
-				execution_message = "autosaved at : " .. vim.fn.strftime("%H:%M:%S"),
-				events = { "FocusLost" },
-				conditions = {
-					exists = true,
-					filetype_is_not = {},
-					modifiable = true,
-				},
-				clean_command_line_interval = 2500,
-				on_off_commands = true,
-				write_all_buffers = false,
-			})
+			require("autosave").setup()
 		end,
 	},
 
