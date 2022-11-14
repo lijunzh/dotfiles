5a439269cf2a1dfc0ac7ad92809de71e66ef1c39
Lijun Zhu <gatechzhu@gmail.com>
2022-11-14 11:39:22 -0600

    Disable default keys for saving and nvtree.

--- config/nvim/lua/custom/mappings.lua
+++ config/nvim/lua/custom/mappings.lua
@@ -1,9 +1,16 @@
 local M = {}
 
+M.disabled = {
+	n = {
+		["<C-n>"] = "", -- undo NvimTreeToggle, use - instead
+		["<C-s>"] = "", -- undo save buffer, use <leader>w instead
+	}
+}
+
 M.general = {
 	n = {
-		["<leader>w"] = { ":w <CR> ", "   save buffer" },
-		["<leader>k"] = { ":q <CR> ", "   close window" },
+		["<leader>w"] = { ":w <CR> ", "save buffer" },
+		["<leader>k"] = { ":q <CR> ", "close window" },
 		[";"] = { ":", "command mode", opts = { nowait = true } },
 	},
 	i = {
@@ -13,14 +20,14 @@ M.general = {
 
 M.nvimtree = {
 	n = {
-		["-"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" },
+		["-"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
 	},
 }
 
 M.packer = {
 	n = {
-		["<C-p>c"] = { ":PackerClean <CR>", "   packer clean" },
-		["<C-p>s"] = { ":PackerSync <CR>", "   packer sync" },
+		["<C-p>c"] = { ":PackerClean <CR>", "packer clean" },
+		["<C-p>s"] = { ":PackerSync <CR>", "packer sync" },
 	},
 }
 
