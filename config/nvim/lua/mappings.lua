125cc539654bb86d9ae3005a5d8c275da8678837
Lijun Zhu <gatechzhu@gmail.com>
2024-06-26 16:38:41 -0500

    Fix a typing lag issue in insert mode.

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -7,8 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map({ "n", "i", "v" }, "<leader>w", "<cmd> w <cr>", { desc = "Save buffer" })
-map({ "n", "i", "v" }, "<leader>k", "<cmd> q <cr>", { desc = "Close window" })
+map({ "n" }, "<leader>w", "<cmd> w <cr>", { desc = "Save buffer" })
+map({ "n" }, "<leader>k", "<cmd> q <cr>", { desc = "Close window" })
 
 map({ "n" }, "<C-s>", "<cmd>  Lazy sync <cr>", { desc = "Update plugins" })
 
