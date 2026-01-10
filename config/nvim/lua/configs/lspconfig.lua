590d0f3556443abd89a0323c9aec4d630101f7f4
Lijun Zhu <lijun@Lijuns-Mac-Studio.local>
2026-01-10 09:58:08 -0600

    Fix: Restore rust_analyzer and lua_ls LSP configurations
    - Accidentally removed in previous commit
    - rust_analyzer: use clippy for checks, enable all cargo features
    - lua_ls: recognize 'vim' global, load nvim runtime, disable telemetry
    - These configurations are still needed and recommended

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,14 +1,33 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = {
-	"bashls",
+	"ruff",
 	"clangd",
 	"jsonls",
-	"lua_ls",
-	"ruff",
-	"rust_analyzer",
+	"yamlls",
+	"bashls",
 	"taplo",
 	"texlab",
-	"yamlls",
+	"rust_analyzer",
+	"lua_ls",
 }
-vim.lsp.enable
+vim.lsp.enable(servers)
+
+vim.lsp.config("rust_analyzer", {
+	settings = {
+		["rust-analyzer"] = {
+			check = { command = "clippy" },
+			cargo = { allFeatures = true },
+		},
+	},
+})
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			telemetry = { enable = false },
+		},
+	},
+})
