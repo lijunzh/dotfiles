d6d4a6a55f5bd0eb430e35cfdb32018a5580a76f
Lijun Zhu <gatechzhu@gmail.com>
2026-01-10 18:10:37 -0600

    fix: use opts pattern for treesitter config
    Changed treesitter to use lazy.nvim's opts pattern which ensures
    the plugin is loaded before config runs. The config function now
    receives opts as a parameter, which lazy.nvim handles properly.
    
    Also reverted to event-based lazy loading for better startup time.

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -72,46 +72,47 @@ return {
 
     {
         "nvim-treesitter/nvim-treesitter",
-        lazy = false,
+        event = { "BufReadPost", "BufNewFile", "BufNewFile" },
         build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    "css",
-                    "html",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "python",
-                    "rust",
-                    "toml",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "css",
+                "html",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "rust",
+                "toml",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = true,
+                disable = { "python" },
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
                 },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = {
-                    enable = true,
-                    disable = { "python" },
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "gnn",
-                        node_incremental = "grn",
-                        scope_incremental = "grc",
-                        node_decremental = "grm",
-                    },
-                },
-            })
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
         end,
     },
 
