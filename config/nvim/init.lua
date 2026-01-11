ca27d5fca2f6b00c349fff0add79fa0600694438
Lijun Zhu <gatechzhu@gmail.com>
2026-01-10 18:01:57 -0600

    fix: load plugins from specific file instead of directory
    Changed lazy.setup() to import from 'plugins.init' specifically
    instead of loading all files in 'plugins/' directory. This prevents
    lsp.lua (which is a config file, not a plugin spec) from being
    loaded as a plugin spec.
    
    Fixes 'Expected a table of specs' error.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,14 +23,14 @@ vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -46,23 +46,25 @@ require("core.autocmds")
 -- Load Plugins
 -- ============================================================================
 
-require("lazy").setup("plugins", {
-  defaults = { lazy = true },
-  install = { colorscheme = { "gruvbox" } },
-  checker = { enabled = false },
-  change_detection = { notify = false },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+require("lazy").setup({
+    { import = "plugins.init" },
+}, {
+    defaults = { lazy = true },
+    install = { colorscheme = { "gruvbox" } },
+    checker = { enabled = false },
+    change_detection = { notify = false },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "matchit",
+                "matchparen",
+                "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
     },
-  },
 })
